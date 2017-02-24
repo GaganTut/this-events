d81cfb367c54069b7033d419809a775ef5f5a52a
GaganTut <gstut@live.com>
2017-02-23 21:40:43 -1000

    Menus are all showing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 //Secret Menu Challenge
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var restaurantNames = document.getElementsByClassName("name");
 
+for (var i = 0; i < restaurantNames.length; i++){
+	restaurantNames[i].addEventListener("click", showMenu);
+}
+
+function showMenu() {
+	var secretMenu = this.querySelector(".menu");
+	if (secretMenu.style.display === "none"){
+		secretMenu.style.display = "block";
+	} else {
+		secretMenu.style.display = "none";
+	}
+}
